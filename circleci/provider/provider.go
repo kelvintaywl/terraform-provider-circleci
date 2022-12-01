36962e9d6d751e84db4da2d6b5e824fe82758146
kelvintaywl <kelvintaywl@gmail.com>
2022-12-01 18:46:03 +0900

    wip

---
+++ circleci/provider/provider.go
@@ -0,0 +1,121 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// Ensure CircleciProvider satisfies various provider interfaces.
+var _ provider.Provider = &CircleciProvider{}
+var _ provider.ProviderWithMetadata = &CircleciProvider{}
+
+const (
+	defaultHostName string = "https://circleci.com"
+)
+
+// CircleciProvider defines the provider implementation.
+type CircleciProvider struct {
+	// version is set to the provider version on release, "dev" when the
+	// provider is built and ran locally, and "test" when running acceptance
+	// testing.
+	version string
+}
+
+// CircleciProviderModel describes the provider data model.
+type CircleciProviderModel struct {
+	ApiToken types.String `tfsdk:"api_token"`
+	Hostname types.String `tfsdk:"hostname"`
+}
+
+func (p *CircleciProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "circleci"
+	resp.Version = p.version
+}
+
+func (p *CircleciProvider) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		Attributes: map[string]tfsdk.Attribute{
+			"api_token": {
+				MarkdownDescription: "CircleCI user API token",
+				Optional:            true,
+				Type:                types.StringType,
+			},
+			"hostname": {
+				MarkdownDescription: "CircleCI API server hostname",
+				Optional:            true,
+				Type:                types.StringType,
+			},
+		},
+	}, nil
+}
+
+func (p *CircleciProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	var data CircleciProviderModel
+
+	// Check environment variables
+	apiToken := os.Getenv("CIRCLE_TOKEN")
+	hostname := os.Getenv("CIRCLE_HOSTNAME")
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Check configuration data, which should take precedence over
+	// environment variable data, if found.
+	if data.ApiToken.ValueString() != "" {
+		apiToken = data.ApiToken.ValueString()
+	}
+
+	if data.Hostname.ValueString() != "" {
+		hostname = data.Hostname.ValueString()
+	}
+
+	if apiToken == "" {
+		resp.Diagnostics.AddError(
+			"Missing CircleCI user API Token configuration",
+			"While configuring the provider, the CircleCI user API token was not found in "+
+				"the CIRCLE_TOKEN environment variable or provider "+
+				"configuration block api_token attribute.",
+		)
+		// Not returning early allows the logic to collect all errors.
+	}
+
+	if hostname == "" {
+		hostname = defaultHostName
+		resp.Diagnostics.AddWarning(
+			"Missing CircleCI API hostname configuration",
+			"While configuring the provider, the CircleCI API hostname was not found in "+
+				"the CIRCLE_HOSTNAME environment variable or provider "+
+				fmt.Sprintf("configuration block hostname attribute.\nUsing default: %s", hostname),
+		)
+	}
+}
+
+func (p *CircleciProvider) Resources(ctx context.Context) []func() resource.Resource {
+	return []func() resource.Resource{
+		NewExampleResource,
+	}
+}
+
+func (p *CircleciProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{
+		NewExampleDataSource,
+	}
+}
+
+func New(version string) func() provider.Provider {
+	return func() provider.Provider {
+		return &CircleciProvider{
+			version: version,
+		}
+	}
+}
---
+++ circleci/provider/schedule_resource.go
@@ -0,0 +1,355 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	api "github.com/kelvintaywl/circleci-schedule-go-sdk/client"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces
+var _ resource.Resource = &ScheduleResource{}
+var _ resource.ResourceWithImportState = &ScheduleResource{}
+
+var DayOfWeek = [7]string{
+	"MON",
+	"TUE",
+	"WED",
+	"THU",
+	"FRI",
+	"SAT",
+	"SUN",
+}
+var Month = [12]string{
+	"JAN",
+	"FEB",
+	"MAR",
+	"APR",
+	"MAY",
+	"JUN",
+	"JUL",
+	"AUG",
+	"SEP",
+	"OCT",
+	"NOV",
+	"DEC",
+}
+var AttributionActor = [2]string{
+	"system",
+	"current",
+}
+
+func NewScheduleResource() resource.Resource {
+	return &ScheduleResource{}
+}
+
+// ScheduleResource defines the resource implementation.
+type ScheduleResource struct {
+	client *api.Circleci
+}
+
+// ScheduleResourceModel describes the resource data model.
+type ScheduleResourceModel struct {
+	Id               types.String `tfsdk:"id"`
+	CreatedAt        types.String `tfsdk:"created_at"`
+	UpdatedAt        types.String `tfsdk:"updated_at"`
+	Name             types.String `tfsdk:"name"`
+	Description      types.String `tfsdk:"description"`
+	ProjectSlug      types.String `tfsdk:"project_slug"`
+	AttributionActor types.String `tfsdk:"attribution_actor"`
+	Actor            types.Object `tfsdk:"actor"`
+	Parameters       types.Object `tfsdk:"parameters"`
+	Timetable        types.Object `tfsdk:"timetable"`
+}
+
+func (r *ScheduleResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_schedule"
+}
+
+func (r *ScheduleResource) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	s := tfsdk.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "CircleCI schedule resource",
+		Attributes: map[string]tfsdk.Attribute{
+			"id": {
+				// read-only
+				Computed:            true,
+				Optional:            false,
+				MarkdownDescription: "The unique ID of the schedule",
+				PlanModifiers: []tfsdk.AttributePlanModifiers{
+					resource.UseStateForUnknown(),
+				},
+				Type: types.StringType,
+			},
+			"created_at": {
+				// read-only
+				Computed:            true,
+				Optional:            false,
+				MarkdownDescription: "The date and time the schedule was created",
+				PlanModifiers: []tfsdk.AttributePlanModifiers{
+					resource.UseStateForUnknown(),
+				},
+				Type: types.StringType,
+			},
+			"updated_at": {
+				// read-only
+				Computed:            true,
+				Optional:            false,
+				MarkdownDescription: "The date and time the schedule was last updated",
+				PlanModifiers: []tfsdk.AttributePlanModifiers{
+					resource.UseStateForUnknown(),
+				},
+				Type: types.StringType,
+			},
+			"actor": {
+				MarkdownDescription: "The actor for the schedule",
+				// read-only
+				Computed: true,
+				Optional: false,
+				Attributes: tfsdk.SingleNestedAttributes(
+					map[string]tfsdk.Attribute{
+						"id": {
+							Type:                types.StringType,
+							MarkdownDescription: "The user ID of the actor",
+						},
+						"login": {
+							Type:                types.StringType,
+							MarkdownDescription: "The user login name of the actor",
+						},
+						"name": {
+							Type:                types.StringType,
+							MarkdownDescription: "The name of the actor",
+						},
+					},
+				),
+			},
+			"name": {
+				MarkdownDescription: "Name of the schedule",
+				Required:            true,
+				Type:                types.StringType,
+			},
+			"description": {
+				MarkdownDescription: "Description of the schedule",
+				Required:            true,
+				Type:                types.StringType,
+			},
+			"project_slug": {
+				MarkdownDescription: "The project-slug for the schedule",
+				Required:            true,
+				Type:                types.StringType,
+			},
+			"parameters": {
+				MarkdownDescription: "Pipeline parameters for the schedule",
+				Required:            true,
+				Attributes: tfsdk.SingleNestedAttributes(
+					map[string]tfsdk.Attribute{
+						"branch": {
+							Type:                types.StringType,
+							Optional:            true,
+							MarkdownDescription: "Branch name for schedule to trigger on",
+						},
+						"tag": {
+							Type:                types.StringType,
+							Optional:            true,
+							MarkdownDescription: "Git tag for schedule to trigger on",
+						},
+						"json": {
+							Type:                types.StringType,
+							Optional:            true,
+							MarkdownDescription: "Additional parameters for pipeline (in JSON string)",
+						},
+					},
+				),
+			},
+			"timetable": {
+				MarkdownDescription: "The timetable for the schedule",
+				Required:            true,
+				Attributes: tfsdk.SingleNestedAttributes(
+					map[string]tfsdk.Attribute{
+						"per_hour": {
+							Type:                types.Int64Type,
+							Required:            true,
+							MarkdownDescription: "Repeats Per Hour",
+							Validators: []tfsdk.AttributeValidator{
+								int64validator.Between(1, 12),
+							},
+						},
+						"days_of_week": {
+							Type:                types.ListType{ElemType: types.StringType},
+							Optional:            true,
+							MarkdownDescription: "Repeats on these days of the week",
+							Validators: []tfsdk.AttributeValidator{
+								listvalidator.ValuesAre(
+									stringvalidator.OneOf(DayOfWeek...),
+								),
+							},
+						},
+						"days_of_month": {
+							Type:                types.ListType{ElemType: types.Int64Type},
+							Optional:            true,
+							MarkdownDescription: "Repeats on these days of the month",
+							Validators: []tfsdk.AttributeValidator{
+								listvalidator.ValuesAre(
+									int64validator.Between(1, 31),
+								),
+							},
+						},
+						"months": {
+							Type:                types.ListType{ElemType: types.Int64Type},
+							Optional:            true,
+							MarkdownDescription: "Repeats on these months",
+							Validators: []tfsdk.AttributeValidator{
+								listvalidator.ValuesAre(
+									stringvalidator.OneOf(Month...),
+								),
+							},
+						},
+					},
+				),
+			},
+			"attribution_actor": {
+				MarkdownDescription: "The attribution-actor of the scheduled pipeline",
+				Required:            true,
+				Type:                types.StringType,
+				Validators: []tfsdk.AttributeValidator{
+					stringvalidator.OneOf(AttributionActor...),
+				},
+			},
+		},
+	}
+	return s, nil
+}
+
+func (r *ScheduleResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*api.Circleci)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *api.Circleci, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	r.client = client
+}
+
+func (r *ScheduleResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data *ScheduleResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := d.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to create example, got error: %s", err))
+	//     return
+	// }
+
+	// For the purposes of this example code, hardcoding a response value to
+	// save into the Terraform state.
+	data.Id = types.StringValue("example-id")
+
+	// Write logs using the tflog package
+	// Documentation: https://terraform.io/plugin/log
+	tflog.Trace(ctx, "created a resource")
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *ScheduleResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data *ScheduleResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Prevent panic if the provider has not been configured.
+	if r.client == nil {
+		resp.Diagnostics.AddError(
+			"Unconfigured CircleCI API Client",
+			"Expected configured CircleCI API client. Please report this issue to the provider developers.",
+		)
+
+		return
+	}
+
+	r.client.
+
+		// Save updated data into Terraform state
+		resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *ScheduleResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data *ScheduleResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := d.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to update example, got error: %s", err))
+	//     return
+	// }
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *ScheduleResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data *ScheduleResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := d.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to delete example, got error: %s", err))
+	//     return
+	// }
+}
+
+func (r *ScheduleResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
