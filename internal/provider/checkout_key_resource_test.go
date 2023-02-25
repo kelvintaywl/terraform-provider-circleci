b5188c15587d9159b5346c7190f1e589f739cc9b
kelvintaywl <kelvintaywl@gmail.com>
2023-02-25 22:03:50 +0900

    test: add update test for checkout key (resource)

--- internal/provider/checkout_key_resource_test.go
+++ internal/provider/checkout_key_resource_test.go
@@ -28,7 +28,24 @@ resource "circleci_checkout_key" "my_key" {
 					resource.TestCheckResourceAttrSet("circleci_checkout_key.my_key", "id"),
 				),
 			},
-			// Update and Read testing skipped; Checkout keys cannot be updated.
+			// Update and Read testing
+			{
+				Config: providerConfig + fmt.Sprintf(`
+			resource "circleci_checkout_key" "my_key" {
+			project_slug = "%s"
+			type         = "user-key"
+			}
+			`, projectSlug),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("circleci_checkout_key.my_key", "type", "user-key"),
+					resource.TestCheckResourceAttr("circleci_checkout_key.my_key", "project_slug", projectSlug),
+					resource.TestCheckResourceAttr("circleci_checkout_key.my_key", "preferred", "true"),
+					// TODO: test these values are changed
+					resource.TestCheckResourceAttrSet("circleci_checkout_key.my_key", "fingerprint"),
+					resource.TestCheckResourceAttrSet("circleci_checkout_key.my_key", "public_key"),
+					resource.TestCheckResourceAttrSet("circleci_checkout_key.my_key", "id"),
+				),
+			},
 		},
 	})
 }
