5137c182a62de6a98117d15d9efa423aa3d950e9
kelvintaywl <kelvintaywl@gmail.com>
2023-02-25 22:46:15 +0900

    test: skip checkout key tests for now
    s

--- internal/provider/checkout_key_resource_test.go
+++ internal/provider/checkout_key_resource_test.go
@@ -1,34 +1,38 @@
 package provider
 
 import (
-	"fmt"
+	_ "fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	_ "github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccProjectCheckoutKeyResource(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			// Create and Read testing
-			{
-				Config: providerConfig + fmt.Sprintf(`
-resource "circleci_checkout_key" "my_key" {
-	project_slug = "%s"
-	type         = "deploy-key"
-}
-`, projectSlug),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("circleci_checkout_key.my_key", "type", "deploy-key"),
-					resource.TestCheckResourceAttr("circleci_checkout_key.my_key", "project_slug", projectSlug),
-					resource.TestCheckResourceAttr("circleci_checkout_key.my_key", "preferred", "true"),
-					resource.TestCheckResourceAttrSet("circleci_checkout_key.my_key", "fingerprint"),
-					resource.TestCheckResourceAttrSet("circleci_checkout_key.my_key", "public_key"),
-					resource.TestCheckResourceAttrSet("circleci_checkout_key.my_key", "id"),
-				),
-			},
-			// Update and Read testing skipped
-		},
-	})
+	t.Skip("disabling tests here, to prevent pollution of deploy keys on GitHub")
+
+	//	resource.Test(t, resource.TestCase{
+	//		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+	//		Steps: []resource.TestStep{
+	//			// Create and Read testing
+	//			{
+	//				Config: providerConfig + fmt.Sprintf(`
+	//
+	//	resource "circleci_checkout_key" "my_key" {
+	//		project_slug = "%s"
+	//		type         = "deploy-key"
+	//	}
+	//
+	// `, projectSlug),
+	//
+	//				Check: resource.ComposeAggregateTestCheckFunc(
+	//					resource.TestCheckResourceAttr("circleci_checkout_key.my_key", "type", "deploy-key"),
+	//					resource.TestCheckResourceAttr("circleci_checkout_key.my_key", "project_slug", projectSlug),
+	//					resource.TestCheckResourceAttr("circleci_checkout_key.my_key", "preferred", "true"),
+	//					resource.TestCheckResourceAttrSet("circleci_checkout_key.my_key", "fingerprint"),
+	//					resource.TestCheckResourceAttrSet("circleci_checkout_key.my_key", "public_key"),
+	//					resource.TestCheckResourceAttrSet("circleci_checkout_key.my_key", "id"),
+	//				),
+	//			},
+	//		},
+	//	})
 }
