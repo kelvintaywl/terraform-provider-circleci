486cb1ce901fadf0b9a8461f581744e98dd9e750
kelvintaywl <kelvintaywl@gmail.com>
2023-02-25 20:07:40 +0900

    test: add tests for checkout keys (data source)

---
+++ internal/provider/checkout_keys_data_source_test.go
@@ -0,0 +1,28 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccCheckoutKeysDataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Read testing
+			{
+				// github/kelvintaywl-cci/tf-provider-acceptance-test-dummy
+				Config: providerConfig + fmt.Sprintf(`
+data "circleci_checkout_keys" "keys" {
+  project_slug = "%s"
+}`, projectSlug),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.circleci_checkout_keys.keys", "id", projectSlug),
+					resource.TestCheckResourceAttr("data.circleci_checkout_keys.keys", "project_slug", projectSlug),
+				),
+			},
+		},
+	})
+}
