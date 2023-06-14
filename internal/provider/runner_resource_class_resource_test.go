bc4e4efd5132f085bb712ec066bc324f6754301e
kelvintaywl <kelvintaywl@gmail.com>
2023-06-14 22:22:25 +0900

    test: add test for Runner resource-class resource

---
+++ internal/provider/runner_resource_class_resource_test.go
@@ -0,0 +1,30 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccRunnerResourceClassResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: providerConfig + fmt.Sprintf(`
+resource "circleci_runner_resource_class" "blade_runner" {
+	resource_class = "%s/acceptance-test"
+	description    = "From Terraform acceptance test"
+}
+`, namespace),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("circleci_runner_resource_class.blade_runner", "resource_class", "kelvintaywl-tf/acceptance-test"),
+					resource.TestCheckResourceAttr("circleci_runner_resource_class.blade_runner", "description", "From Terraform acceptance test"),
+					resource.TestCheckResourceAttrSet("circleci_runner_resource_class.blade_runner", "id"),
+				),
+			},
+		},
+	})
+}
